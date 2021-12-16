e4a18d92160934208cac3dae7770b1fec7f03173
Elena Andreeva <elena@madebyforward.com>
2021-12-16 20:30:07 +0300

    Add FeedbackForm input onChange validation

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -4,9 +4,21 @@ import Button from "./shared/Button";
 
 function FeedbackForm() {
   const [text, setText] = useState("");
+  const [btnDisabled, setBtnDisabled] = useState(true);
+  const [message, setMessage] = useState("");
 
   const handleTextChange = (e) => {
-    setText(e.target.value);
+      if(text === '') {
+        setBtnDisabled(true);
+        setMessage(null);
+      } else if(text !== '' && text.trim().length <= 10) {
+          setMessage('Text must be at least 10 characters');
+        setBtnDisabled(true);
+      } else {
+        setMessage(null);
+        setBtnDisabled(false);
+      }
+      setText(e.target.value)
   };
 
   return (
@@ -20,8 +32,10 @@ function FeedbackForm() {
             placeholder="Write a review"
             value={text}
           />
-          <Button type='submit' version='secondary'>Send</Button>
+          <Button type='submit' version='secondary' isDisabled={btnDisabled}>Send</Button>
         </div>
+
+        {message && <div className="message">{message}</div>}
       </form>
     </Card>
   );
