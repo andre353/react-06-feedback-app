a6a789cd08d67c483236397a9ea25c96ca82c10e
Elena Andreeva <elena@madebyforward.com>
2021-12-17 19:53:46 +0300

    Add AboutPage, AboutIconLink, Post components

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,25 @@
+import Card from '../components/shared/Card'
+import {Link, NavLink} from 'react-router-dom'
+
+function AboutPage() {
+    return (
+        <>
+            <Card>
+                <NavLink to='/' className="nav-link" activeClassName='active'>Home</NavLink>
+                <NavLink to='/about' className="nav-link" activeClassName='active'>About</NavLink>
+            </Card>
+            <Card>
+                <div className="about">
+                    <h1>About This Project</h1>
+                    <p>This is a React app to leave feedback for a product or service</p>
+                    <p>Version: 1.0.0</p>
+                    <p>
+                        <Link to='/'>Back To Home</Link>
+                    </p>
+                </div>
+            </Card>
+        </>
+    )
+}
+
+export default AboutPage
