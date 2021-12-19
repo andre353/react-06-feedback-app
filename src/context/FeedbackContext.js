e542d1aaf4d4eea6a2477365fb724771be4eac34
andre353 <eva595eva@gmail.com>
2021-12-19 19:44:11 +0300

    Add proxy rule to package.json, add json db data deletion and update via fetch

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,4 +1,3 @@
-import { v4 as uuidv4 } from "uuid";
 import { createContext, useState, useEffect } from "react";
 
 const FeedbackContext = createContext();
@@ -19,7 +18,7 @@ export const FeedbackProvider = ({ children }) => {
 
   // Fetch feedback
   const fetchFeedback = async () => {
-      const response = await fetch(`http://localhost:5000/feedback?_sort=id&_order=desc`)
+      const response = await fetch(`/feedback?_sort=id&_order=desc`)
       // The json() method of the Response interface takes a Response stream and reads it to completion. It returns a promise which resolves with the result of parsing the body text as JSON.
       const data = await response.json()
       // assigning fetched data to feedback variable
@@ -27,26 +26,40 @@ export const FeedbackProvider = ({ children }) => {
       setIsLoading(false)
   }
 
-  const deleteFeedbackItem = (id) => {
+  const deleteFeedbackItem = async (id) => {
     if (window.confirm("Are you sure you want to delete this item?")) {
+      await fetch(`/feedback/${id}`, { method: 'DELETE'})
       //  returning a new array - the old one stays untouched - based on condition that the item that we click is not equal to the item in the array, once it is equal - it is out of the new array = the new array consists only of not-clicked items
       setFeedback(feedback.filter((item) => item.id !== id));
     }
   };
 
-  const addFeedback = (newFeedback) => {
-    // let's set a unique id to each newly generated feedback item object
-    newFeedback.id = uuidv4();
-    // to update the state, we can't just push the new feedback item on it, as it is immutable
-    // we need basically make a copy of it - we get the current objects that are already there via spread operator ...
-    // and on addition to those at the very beginnng of the array we put our new item=object
-    setFeedback([newFeedback, ...feedback]);
-  };
+  const addFeedback = async (newFeedback) => {
+    const response = await fetch('/feedback', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(newFeedback)
+    })
+    const data = await response.json()
+    // json npm autoincrements - for id
+    setFeedback([data, ...feedback])
+  }
 
   // Update feedback item after editting it
-  const updateFeedback = (id, updItem) => {
+  const updateFeedback = async (id, updItem) => {
+    const response = await fetch(`/feedback/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(updItem)
+    })
+
+    const data = await response.json()
       // ids compared, if the current id already exists in feedback.item - item object will be only updated with the fields changed, otherwise we just add the new object
-      setFeedback(feedback.map((item) => item.id === id ? { ...item, ...updItem } : item))
+      setFeedback(feedback.map((item) => item.id === id ? { ...item, ...data } : item))
   }
 
   // set the clicked item to be updated via it's edit
