4b4e1e89e1d1e7d50873b26a5e55f56adeb7a1b1
Elena Andreeva <elena@madebyforward.com>
2021-12-19 16:31:17 +0300

    Add Spinner component to be used in FeedbackList

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,31 +1,31 @@
-import {motion, AnimatePresence} from "framer-motion";
-import {useContext} from 'react';
+import { motion, AnimatePresence } from "framer-motion";
+import { useContext } from "react";
 import FeedbackItem from "./FeedbackItem";
-import FeedbackContext from '../context/FeedbackContext';
+import Spinner from "./shared/Spinner";
+import FeedbackContext from "../context/FeedbackContext";
 
 function FeedbackList() {
-  const { feedback } = useContext(FeedbackContext)
+  const { feedback, isLoading } = useContext(FeedbackContext);
 
-  if (!feedback || feedback.length === 0) {
+  if (!isLoading && (!feedback || feedback.length === 0)) {
     return <p>No feedback yet</p>;
   }
 
-  return (
+  return isLoading ? (
+    <Spinner />
+  ) : (
     <div className="feedback-list">
       <AnimatePresence>
-      {feedback.map((item) => (
-        <motion.div 
-        key={item.id}
-        initial={{opacity: 0}}
-        animate={{opacity: 1}}
-        exit={{opacity: 0}}
-        >
-        <FeedbackItem
-          key={item.id}
-          item={item}
-        />
-        </motion.div>
-      ))}
+        {feedback.map((item) => (
+          <motion.div
+            key={item.id}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            <FeedbackItem key={item.id} item={item} />
+          </motion.div>
+        ))}
       </AnimatePresence>
     </div>
   );
@@ -34,7 +34,7 @@ function FeedbackList() {
 // FeedbackList.propTypes = {
 //   feedback: PropTypes.arrayOf(
 //     PropTypes.shape({
-      //id: PropTypes.number.isRequired,
+//id: PropTypes.number.isRequired,
 //       text: PropTypes.string.isRequired,
 //       rating: PropTypes.number.isRequired,
 //     })
--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -3,7 +3,7 @@ import { createContext, useState, useEffect } from "react";
 
 const FeedbackContext = createContext();
 
-// in order the components to get the context, they need to be wrapped in a Provider in App.js
+// in order the components to get the context, they need to be wrapped in a "createContext().Provider" in App.js
 export const FeedbackProvider = ({ children }) => {
   const [isLoading, setIsLoading] = useState(true)  
   const [feedback, setFeedback] = useState([]);
@@ -12,7 +12,7 @@ export const FeedbackProvider = ({ children }) => {
       edit: false
   })
 
-  // we want the data to be fetched as soon as the pahe loads, to be used once that's why the 2-nd argument as a array of dependancies is empty
+  // we want the data to be fetched as soon as the page loads, to be used once that's why the 2-nd argument as an array of dependancies is empty
   useEffect(() => {
       fetchFeedback()
   }, [])
@@ -20,7 +20,9 @@ export const FeedbackProvider = ({ children }) => {
   // Fetch feedback
   const fetchFeedback = async () => {
       const response = await fetch(`http://localhost:5000/feedback?_sort=id&_order=desc`)
+      // The json() method of the Response interface takes a Response stream and reads it to completion. It returns a promise which resolves with the result of parsing the body text as JSON.
       const data = await response.json()
+      // assigning fetched data to feedback variable
       setFeedback(data);
       setIsLoading(false)
   }
@@ -33,21 +35,21 @@ export const FeedbackProvider = ({ children }) => {
   };
 
   const addFeedback = (newFeedback) => {
+    // let's set a unique id to each newly generated feedback item object
     newFeedback.id = uuidv4();
-    // let's set a unique id to each newly generated feedback item
     // to update the state, we can't just push the new feedback item on it, as it is immutable
     // we need basically make a copy of it - we get the current objects that are already there via spread operator ...
     // and on addition to those at the very beginnng of the array we put our new item=object
     setFeedback([newFeedback, ...feedback]);
   };
 
-  // Update feedback item
+  // Update feedback item after editting it
   const updateFeedback = (id, updItem) => {
-      // if the current id was already in feedback.item we add to the array the updated item object, otherwise we just add the new object
+      // ids compared, if the current id already exists in feedback.item - item object will be only updated with the fields changed, otherwise we just add the new object
       setFeedback(feedback.map((item) => item.id === id ? { ...item, ...updItem } : item))
   }
 
-  // set the clicked item to be updated via editting it
+  // set the clicked item to be updated via it's edit
   const editFeedback = (item) => {
       setFeedbackEdit({
           item,
